bd69fe1776bbf99e150dc3bcfc6cf6c12ea7279f
sgulhane02 <30700958+sgulhane02@users.noreply.github.com>
2023-12-04 22:49:50 +0530

    Product Module Entity implementation done, Successfully.

---
+++ src/main/java/com/BikkadIT/electronic/store/entities/Product.java
@@ -0,0 +1,46 @@
+package com.BikkadIT.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    private String productId;
+
+    @Column(name = "product_title")
+    private String title;
+
+    @Column(name = "product_description")
+    private String description;
+
+    @Column(name = "product_price")
+    private Double price;
+
+    @Column(name = "product_discounted_price")
+    private Double discountedPrice;
+
+    @Column(name = "product_quantity")
+    private Integer quantity;
+
+    @Column(name = "product_added_date")
+    private Date addedDate;
+
+    @Column(name = "product_live")
+    private Boolean live;
+
+    @Column(name = "product_stock")
+    private Boolean stock;
+}
