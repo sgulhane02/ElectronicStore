f64800bed53a88ecdf9702a0b4649cbf9db3dcd5
sgulhane02 <30700958+sgulhane02@users.noreply.github.com>
2023-12-13 15:26:01 +0530

    Service Category, Create Category, Testing & Junit Test cases implementation done, Successfully.

---
+++ src/test/java/com/BikkadIT/electronic/store/services/CategoryServiceTest.java
@@ -0,0 +1,49 @@
+package com.BikkadIT.electronic.store.services;
+
+import com.BikkadIT.electronic.store.dtos.CategoryDto;
+import com.BikkadIT.electronic.store.entities.Category;
+import com.BikkadIT.electronic.store.repositories.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class CategoryServiceTest {
+
+    @MockBean
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private Category category;
+
+    @BeforeEach
+    public void init() {
+
+        category = Category
+                .builder()
+                .title("Mobile phones")
+                .description("this is related to phones")
+                .coverImage("mob.png")
+                .build();
+    }
+
+    @Test
+    public void createCategoryTest(){
+
+        Mockito.when(categoryRepository.save(Mockito.any())).thenReturn(category);
+        CategoryDto category1 = categoryService.createCategory(modelMapper.map(category, CategoryDto.class));
+        Assertions.assertNotNull(category1);
+        Assertions.assertEquals("Mobile phones",category1.getTitle());
+
+    }
+}
