3c23c938cda83a19ff9fbe27d41f3e1b458fdb60
sgulhane02 <30700958+sgulhane02@users.noreply.github.com>
2023-12-13 16:03:17 +0530

    Service Category, get all Category, Testing & Junit Test cases implementation done, Successfully.

--- src/test/java/com/BikkadIT/electronic/store/services/CategoryServiceTest.java
+++ src/test/java/com/BikkadIT/electronic/store/services/CategoryServiceTest.java
@@ -2,6 +2,7 @@ package com.BikkadIT.electronic.store.services;
 
 import com.BikkadIT.electronic.store.dtos.CategoryDto;
 import com.BikkadIT.electronic.store.entities.Category;
+import com.BikkadIT.electronic.store.payload.PageableResponse;
 import com.BikkadIT.electronic.store.repositories.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,7 +12,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -65,5 +71,27 @@ public class CategoryServiceTest {
 
         CategoryDto updatedCategory = categoryService.updateCategory(categoryDto, categoryId);
         Assertions.assertNotNull(categoryDto);
+    }
+    @Test
+    public void getAllCategoriesTest(){
+
+        Category category1=Category
+                .builder()
+                .title("Mobile phones")
+                .description("this is related to phones")
+                .coverImage("mob.png")
+                .build();
+        Category category2=Category
+                .builder()
+                .title("Mobile phones")
+                .description("this is related to phones")
+                .coverImage("mob.png")
+                .build();
+
+        List<Category> categories = Arrays.asList(category, category1, category2);
+        Page<Category> page=new PageImpl<>(categories);
+        Mockito.when(categoryRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+        PageableResponse<CategoryDto> allCategories = categoryService.getAllCategories(1, 2, "title", "asc");
+        Assertions.assertEquals(3,allCategories.getContent().size());
     }
 }
